3b155cb904379ca9234e1b5c891ce9c18ea1d65c
Felix Centeno <fjch100@gmail.com>
2024-01-17 20:27:38 -0500

    add code for lessons 644 to 647

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from './NewPost';
@@ -6,8 +6,29 @@ import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            setIsLoading(true);
+            let response = await fetch("http://localhost:8080/posts");
+            let data = await response.json();
+            setPosts(data.posts);
+            setIsLoading(false);
+        }
+        fetchData();
+
+    }, []);
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((existingPosts) => [postData, ...existingPosts]);
     }
 
@@ -21,16 +42,19 @@ function PostList({ isPosting, onStopPosting }) {
                     />
                 </Modal>
             )}
-            {posts.length > 0 && <ul className={classes.posts}>
+            {!isLoading && posts.length > 0 && <ul className={classes.posts}>
                 {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
             </ul>
             }
-            {posts.length === 0 &&
+            {!isLoading && posts.length === 0 &&
                 <div style={{ textAlign: 'center', color: 'white' }}>
                     <h2>THere are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
             }
+            {isLoading && <div style={{ textAlign: 'center', color: 'white' }}>
+                <p>Loading...!</p>
+            </div>}
 
         </>
     )
